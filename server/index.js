40947ba5485f63ad1e2759131ad30820c496ef70
kunalpro379 <2022.kunal.patil@ves.ac.in>
2026-01-07 17:55:59 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors({
     if (!origin) return callback(null, true);
 
     if (allowedOrigins.includes(origin)) {
-      return callback(null, origin);
+      return callback(null, true);
     }
 
     return callback(new Error('Not allowed by CORS'));
