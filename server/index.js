8bb9ebf6d42565a16f93623cd9d6498d5d6a6149
kunalpro379 <2022.kunal.patil@ves.ac.in>
2026-01-07 18:08:54 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const allowedOrigins = new Set([
   'http://localhost:3000'
 ]);
 // app.use((req, res, next) => {
-//   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+//   res.setHeader('Cache-Control', ' ye sabno-store, no-cache, must-revalidate, proxy-revalidate');
 //   res.setHeader('Pragma', 'no-cache');
 //   res.setHeader('Expires', '0');
 //   next();
