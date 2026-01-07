4c9461f42e6153ecfed2e668b5e62a946a7a1412
kunalpro379 <2022.kunal.patil@ves.ac.in>
2026-01-07 14:00:06 +0530

    CORS

--- server/index.js
+++ server/index.js
@@ -33,10 +33,13 @@ app.use(cors({
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
+      console.log('CORS blocked origin:', origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
